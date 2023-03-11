abc6587350a9e7d0fed298bc9ce9d250cd42e398
trainee <projetdevops23@gmail.com>
2023-03-11 14:18:50 +0100

    useless file deleted + modif tomcat and postgresql in pre-prod

--- lunch-ansible.sh
+++ lunch-ansible.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 scp -r /home/trainee/DevOps-Project/k8s_ansible/pre-prod trainee@192.168.56.2:/home/trainee/
-scp -r /home/trainee/DevOps-Project/k8s_ansible/prod trainee@192.168.56.2:/home/trainee/
+#scp -r /home/trainee/DevOps-Project/k8s_ansible/prod trainee@192.168.56.2:/home/trainee/
 
 ansible-playbook -i hosts dependancies_k8s.yml
